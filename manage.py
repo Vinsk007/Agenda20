68bcd4a44a0ee48d555eba027b9acf89b9c9f363
Vinsk007 <rodrigo.vinskoski@gmail.com>
2019-12-10 15:21:11 -0300

    Teste Git Agenda20 - Update managepy (teste)
    Signed-off-by: Vinsk007 <rodrigo.vinskoski@gmail.com>

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def main():
+def main(): #teste new commit git 2
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Agenda20.settings')
     try:
         from django.core.management import execute_from_command_line
