3023c27895d64aec6ea2332baf3c9c564759b77a
Vinsk007 <rodrigo.vinskoski@gmail.com>
2019-12-11 07:54:37 -0300

    Teste Git Agenda20 - Criando tabela de eventos, adicionando usuários - 3rd Commit
    Signed-off-by: Vinsk007 <rodrigo.vinskoski@gmail.com>

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from core.models import  Eventos # Importação da tabela de eventos - Aula5
 # Register your models here.
+
+# Cria classe de eventos
+class EventosAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao') # Itens que irão aparecer na lista de eventos
+    list_filter = ('usuario', ) #Adiciona um filtro por itens da lista
+
+# Registrar entradas no arquivo Admin - Aula5
+admin.site.register(Eventos, EventosAdmin) #Deve-se associar a classe EventosAdmin
+
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import  User
 
 # Create your models here.
+
+# Criando novas tabelas de eventos - Projeto Agenda20 - Aula5
+class Eventos(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE) # Insere opção de cadastrar e deletar users (opção CASCADE apaga tudo desse user)
+
+    class Meta:
+        db_table = 'evento' #Cria um options para o noma da tabela de eventos
+
+    def __str__(self):
+        return  self.titulo  # Insere o título do evento na lista de eventos (se não aparece como 'object1'
+
